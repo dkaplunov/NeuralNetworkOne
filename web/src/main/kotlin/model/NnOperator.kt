d6a5190ed18a3d5123ae880a9b27de9101cab4e0
dkaplunov <dkaplunov@centre-it.com>
2018-04-23 14:14:31 +0500

    Kotlin: web model

--- web/src/main/kotlin/model/NnOperator.kt
+++ web/src/main/kotlin/model/NnOperator.kt
@@ -4,6 +4,7 @@ import ru.kda.nn.base.perceptron.PerceptronNetwork
 import ru.kda.nn.functions.activate.Sigmoid
 import ru.kda.nn.functions.error.RootMSE
 import ru.kda.nn.teacher.Teacher
+import ru.kda.nn.teacher.TranerSet
 import ru.kda.nn.teacher.methods.gradient.GradientDescent
 
 class NnOperator (params: NnParams) {
@@ -27,7 +28,10 @@ class NnOperator (params: NnParams) {
                 else -> RootMSE()
             })
 
-    fun teachNN (eduValues: List <eduValue>) {
+    fun teachNN (eduValues: List <EduValue>) {
+        eduValues.forEach {
+            teacher.addTranerSet(TranerSet(toDoubleArray(it.binInitValue), toDoubleArray(it.binAnsverValue)))
+        }
 
     }
 
@@ -35,19 +39,21 @@ class NnOperator (params: NnParams) {
         return toNnValue(nn.executeNetwork(toDoubleArray(params)));
     }
 
-    fun toDoubleArray (inputValue:NnValues): DoubleArray {
+    fun toDoubleArray (inputValue:NnValues): DoubleArray = toDoubleArray(inputValue.binValue)
+
+    fun toDoubleArray (inputValue:Int): DoubleArray {
         val values:MutableList<Double> = ArrayList<Double>()
-        var value = inputValue.binValue
+        var value = inputValue
         while (value > 0) {
             values.add((value%2).toDouble())
             value = value/2
         }
-        return values.toDoubleArray();
+        return values.toDoubleArray().reversedArray();
     }
 
     fun toNnValue (arr:DoubleArray):NnValues {
         var result:Double = 0.0;
-        arr.reversedArray().forEach {
+        arr.forEach {
             result = result*2+it
         }
         return NnValues(result.toInt())
@@ -55,5 +61,5 @@ class NnOperator (params: NnParams) {
 }
 
 data class NnParams (val levels:IntArray, val activFunName:String, val teacher:String, val numEpoche:Int, val errFun:String);
-data class NnValues (var binValue:Int)
-data class eduValue (val binInitValue:Int, val binAnsverValue:Int)
+data class NnValues (val binValue:Int)
+data class EduValue (val binInitValue:Int, val binAnsverValue:Int)
--- web/src/test/kotlin/model/TestModel.kt
+++ web/src/test/kotlin/model/TestModel.kt
@@ -1,13 +1,32 @@
 package model
 import org.junit.Test as test
-import org.junit.Assert
+import org.junit.Before
 
 class TestModel {
 
-    var nnOperator:NnOperator = NnOperator(NnParams(intArrayOf(2,3,1),"_", "_", 100000, "_"))
+    var nnOperator:NnOperator = NnOperator(NnParams(intArrayOf(2,4,1),"_", "_", 100000, "_"))
+
+    @Before fun init () {
+
+    }
+
 
     @test fun doubleToNnValueTest () {
-        val nnValue:NnValues = NnValues(22)
+        val nnValue = NnValues(22)
         assert(nnOperator.toNnValue(nnOperator.toDoubleArray(nnValue)).binValue == nnValue.binValue)
+    }
+
+    @test fun executeNNTest () {
+        doStuding();
+        nnOperator.execute(NnValues(0))
+    }
+
+    fun doStuding () {
+        val studingData = arrayOf (
+                EduValue(0,0),
+                EduValue(1,1),
+                EduValue(2,1),
+                EduValue(3,0))
+        nnOperator.teachNN(studingData.asList())
     }
 }
