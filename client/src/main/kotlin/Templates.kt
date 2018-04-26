4af7f9033511d09e5b905ee806ef0d8a71e67b33
Dmitry Kaplunov <dmitriy.kaplunov@center-it.com>
2018-04-26 19:55:28 +0500

    Client, templates

--- client/src/main/kotlin/Templates.kt
+++ client/src/main/kotlin/Templates.kt
@@ -2,21 +2,25 @@ import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.get
 import org.w3c.dom.parsing.DOMParser
-import kotlin.browser.document
 
 object Templates {
-    fun applayTemplates (rootElement:Element?) {
+    fun loadTemplates (rootElement:Element?) {
         val nl = rootElement!!.getElementsByTagName("template")
         for (i in 0..nl.length-1) {
             val elm =nl.get(i)
             val name = elm!!.attributes.get("name")!!.value
             XHttp.get(name).then (fun (res:String) {
                 val obj = DOMParser().parseFromString(res, "text/html").activeElement!!.firstChild as Node
-                applayTemplates(obj as Element)
+                loadTemplates(obj as Element)
                 elm.replaceWith(obj)
             })
 
         }
-
     }
+
+    fun applyTemplate (obj:dynamic) {
+        return obj["prop1"]
+    }
+
+    data class TestData (val prop1:String, val prop2:Int)
 }
--- client/src/main/kotlin/test_js.kt
+++ client/src/main/kotlin/test_js.kt
@@ -1,15 +1,12 @@
-import org.w3c.dom.Element
-import org.w3c.dom.Node
-import org.w3c.dom.NodeList
-import org.w3c.dom.get
-import org.w3c.dom.parsing.DOMParser
 import kotlin.browser.document
 import kotlin.browser.window
 
 fun main(args: Array<String>) {
 
     window.onload = {
-        Templates.applayTemplates(document.documentElement)
+        Templates.loadTemplates(document.documentElement)
+
+        println(Templates.applyTemplate(Templates.TestData("1", 1)))
     }
 }
 
